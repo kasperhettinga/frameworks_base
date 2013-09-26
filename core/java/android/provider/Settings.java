6242689761b6f51515f2826848b177f28aeb1e02
htchoi1995 <htchoi1995@gmail.com>
2013-09-26 13:01:58 +0200

    [1/2] SystemUI: Network speed indicator
    Omni change id: I88e7cdfe20342c695a33383db36bf80bb4ffe009
    
    Appeared after
    http://forum.xda-developers.com/showpost.php?p=50643911&postcount=3361
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/colors.xml
    
    PS3: Rebase
    
    Change-Id: I2cc12bd888d592ea9c3a401b859b63a254614a18

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3820,6 +3820,12 @@ public final class Settings {
         public static final String CALL_UI_IN_BACKGROUND = "call_ui_in_background";
 
         /**
+         * Network speed indicator
+         * @hide
+         */
+        public static final String STATUS_BAR_TRAFFIC = "status_bar_traffic";
+
+        /**
          * Whether incomming call UI stays in background and shows as heads up notification
          *
          * @hide
