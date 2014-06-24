0cd3a40d30226b8df6da8d2b13c6dcc707943530
BigBrother1984 <stevewatersy@gmail.com>
2014-06-25 00:55:29 +0200

    HOVER: Don't process and/or show foreground app notifications
    - Don't show notification of foreground app
    - Animate the ticker in this case so user can see it
    
    Inspired from changes linked here https://gerrit.paranoidandroid.co/#/c/4453/10
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    Change-Id: I9f45e1641a7478c7e460c32133db00bd67f63e75
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -442,13 +442,18 @@ public abstract class BaseStatusBar extends SystemUI implements
         });
     }
 
+    public NotificationHelper getNotificationHelperInstance() {
+        if (mNotificationHelper == null) mNotificationHelper = new NotificationHelper(this, mContext);
+        return mNotificationHelper;
+    }
+
     public Hover getHoverInstance() {
-        if(mHover == null) mHover = new Hover(this, mContext);
+        if (mHover == null) mHover = new Hover(this, mContext);
         return mHover;
     }
 
     public Peek getPeekInstance() {
-        if(mPeek == null) mPeek = new Peek(this, mContext);
+        if (mPeek == null) mPeek = new Peek(this, mContext);
         return mPeek;
     }
 
